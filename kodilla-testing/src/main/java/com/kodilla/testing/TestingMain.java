33f34ab8b8a14e59cbba4d475bc4212bdda33880
Twoje Imię i Nazwisko <dawindo317@gmail.com>
2025-10-10 09:10:22 +0200

    6.2 Added user and calculator Testing

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,43 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+
+        System.out.println("Test - drugi test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+
+        int AddingTest = calculator.add(1, 2);
+        if (AddingTest == 3) {
+            System.out.println("Adding test OK");
+        }
+        else{
+            System.out.println("Adding Error!");
+        }
+
+        int SubtractionTest = calculator.sub(1, 2);
+        if (SubtractionTest == -1) {
+            System.out.println("Subtraction test OK");
+        }
+        else{
+            System.out.println("Subtraction Error!");
+        }
     }
 }
