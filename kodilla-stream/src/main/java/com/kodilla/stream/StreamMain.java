7702ec84c0b62bf114fcd3503b407ff853612a40
Twoje Imię i Nazwisko <dawindo317@gmail.com>
2025-10-23 00:51:08 +0200

    7.2 added immutable and iterate package

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -31,5 +31,8 @@ public class StreamMain {
         poemBeautifier.beautify("Initial poem", (poem) -> "ABC" + poem + "ABC");
         poemBeautifier.beautify("Initial poem", (poem) -> poem.toLowerCase());
         poemBeautifier.beautify("Initial poem", (poem) -> poem.replace(" ", "-"));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
