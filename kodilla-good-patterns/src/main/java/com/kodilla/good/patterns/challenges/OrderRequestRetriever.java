d09b9e6b495ce945db5ce23a87b4e998f5edeb18
Twoje Imię i Nazwisko <dawindo317@gmail.com>
2025-11-01 16:34:51 +0100

    12.2.1 deleted unnecessary comment

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderRequestRetriever.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderRequestRetriever.java
@@ -8,19 +8,4 @@ public class OrderRequestRetriever {
         Product toothbrush = new Product("toothbrush", "12345");
         return new OrderRequest(user1, toothbrush, 2);
     }
-
-    /*
-    other examples
-    User user1 = new User("Mateusz", "Kowalski",
-            "MateruszKowalski@gmail.com", "Sienkiewicza 3");
-    User user2 = new User("Daniel", "Kowalski",
-            "DanielKowalski@gmail.com",  "Warszawska 4");
-
-    Product toothbrush = new Product("toothbrush", "12345");
-    Product thongs = new Product("thongs", "67890");
-
-    OrderRequest orderRequest1 = new OrderRequest(user1, toothbrush, 2);
-    OrderRequest orderRequest2 = new OrderRequest(user2, thongs, 1);
-    OrderRequest orderRequest3 = new OrderRequest(user1, thongs, 1);
-     */
 }
